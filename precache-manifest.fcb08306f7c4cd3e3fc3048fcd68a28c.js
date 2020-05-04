d6768ab640cce75d3388a14e2cd58fc1720c0115
Harshil Goel <HarshilGoel@example.com>
2020-05-04 16:49:25 +0530

    Updates

---
+++ precache-manifest.fcb08306f7c4cd3e3fc3048fcd68a28c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd14264931abad06f5eeccb33b6b3db3",
+    "url": "/ToDoApplication/index.html"
+  },
+  {
+    "revision": "ff6ee152d69ab7d5f801",
+    "url": "/ToDoApplication/static/css/main.f188df0b.chunk.css"
+  },
+  {
+    "revision": "95caf6567e13071f2a4e",
+    "url": "/ToDoApplication/static/js/2.e07def1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoApplication/static/js/2.e07def1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff6ee152d69ab7d5f801",
+    "url": "/ToDoApplication/static/js/main.22a5f820.chunk.js"
+  },
+  {
+    "revision": "52218141f64b61901999",
+    "url": "/ToDoApplication/static/js/runtime-main.f30045fa.js"
+  }
+]);
